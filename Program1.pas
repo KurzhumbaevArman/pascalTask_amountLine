1fd7a2dca22c157880dee493082fcf5056b65fd3
KurzhumbaevArman <arman074.95@mail.ru>
2016-03-06 22:39:11 +0500

    Update Program1.pas

--- Program1.pas
+++ Program1.pas
@@ -3,14 +3,14 @@ const
   SzN = 26; 
  
 type Matrix = array [1..SzM,1..SzN] of integer;
-procedure FillMatrixByRandom(var a: Matrix; m,n: integer); // Заполнение случайными
+procedure FillMatrixByRandom(var a: Matrix; m,n: integer); // Р—Р°РїРѕР»РЅРµРЅРёРµ РјР°С‚СЂРёС†С‹ СЃР»СѓС‡Р°Р№РЅС‹РјРё С‡РёСЃР»Р°РјРё
 begin
   for var i:=1 to M do 
   for var j:=1 to N do 
     a[i,j] := Random(10);
 end;
  
-procedure PrintMatrix(const a: Matrix; m,n: integer); // Вывод матрицы
+procedure PrintMatrix(const a: Matrix; m,n: integer); // Р’С‹РІРѕРґ РјР°С‚СЂРёС†С‹
 begin
   for var i:=1 to M do 
   begin
@@ -28,7 +28,7 @@ begin
   var n := 7;
   FillMatrixByRandom(a,m,n);
  
-  writeln('Исходная матрица: ');
+  writeln('РњР°С‚СЂРёС†Р° РёСЃС…РѕРґРЅР°СЏ: ');
   PrintMatrix(a,m,n);
  
   for var i:=1 to m do
@@ -39,7 +39,7 @@ begin
     sums[i] := sum;    
   end;
  
-  writeln('Суммы в строках: ');
+  writeln('РЎСѓРјРјС‹ РІ СЃС‚СЂРѕРєР°С…: ');
   for var i:=1 to m do
     write(sums[i]:4);
 end.
